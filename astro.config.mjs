7e9dfd98f3e3224d12013ba24a7bdeb9336e18f4
Bryan Cardenas <brayansti34@gmail.com>
2026-08-11 12:03:02 -0500

    refactor: reemplazar nav react por astro vanilla

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +10,4 @@ export default defineConfig({
   site: 'https://brayancardenas.vercel.app',
   base: '/',
   output: 'static',
-  integrations: [react()],
 });
